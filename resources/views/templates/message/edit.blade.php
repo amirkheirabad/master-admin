2f6984ff7235b5c2f9113a29ecb44af6f8a29023
sinas <sinagh22831@gmail.com>
2026-05-16 10:26:29 +0330

    message-style-fix

--- resources/views/templates/message/edit.blade.php
+++ resources/views/templates/message/edit.blade.php
@@ -18,7 +18,7 @@
                     <div class="row">
                         <div class="col-md-12 mb-3">
                             <label class="fw-bold">عنوان</label>
-                            <input type="text" name="title" id="title" class="form-control" value="{{ $message->title }}">
+                            <input type="text" name="title" id="title" class="form-control custom-radius input-border-focus" style="background: #f9fafb;" value="{{ $message->title }}">
                             <div class="mt-1">
                                 <span class="text-danger error-message" id="title_error"></span>
                             </div>
@@ -26,7 +26,7 @@
 
                         <div class="col-md-12 mb-3">
                             <label class="fw-bold">متن پیام</label>
-                            <textarea name="content" id="content" class="form-control" rows="5">{{ $message->content }}</textarea>
+                            <textarea name="content" id="content" class="form-control custom-radius input-border-focus" style="background: #f9fafb;" rows="5">{{ $message->content }}</textarea>
                             <div class="mt-1">
                                 <span class="text-danger error-message" id="content_error"></span>
                             </div>
@@ -34,7 +34,7 @@
 
                         <div class="col-md-6 mb-3">
                             <label class="fw-bold">وضعیت</label>
-                            <select name="is_active" id="is_active" class="form-control">
+                            <select name="is_active" id="is_active" class="form-control custom-radius custom-select-input input-border-focus" style="background: #f9fafb;">
                                 <option value="1" {{ $message->is_active ? 'selected' : '' }}>فعال</option>
                                 <option value="0" {{ !$message->is_active ? 'selected' : '' }}>غیرفعال</option>
                             </select>
@@ -42,7 +42,7 @@
 
                         <div class="col-md-6 mb-3">
                             <label class="fw-bold">ترتیب نمایش</label>
-                            <input type="number" name="order" id="order" class="form-control" value="{{ $message->order ?? 0 }}">
+                            <input type="number" name="order" id="order" class="form-control custom-radius input-border-focus" style="background: #f9fafb;" value="{{ $message->order ?? 0 }}">
                         </div>
 
                         <div class="d-flex justify-content-end col-md-12 mt-8">
